b8051b2960790063bb58d0ea916b6db5f84e5e26
povadok <82115304+povadok@users.noreply.github.com>
2022-03-29 00:17:21 +0300

    Update scrypt1.js

--- scrypt1.js
+++ scrypt1.js
@@ -3,7 +3,7 @@
 let input;
 input = prompt ('Введите число');
 input = +input;
-if (isNaN(input) || typeof input !== 'Number') {
+if (isNaN(input) || typeof input !== 'number') {
   alert ('Упс, кажется, вы ошиблись')
 } else if (input %2 == 0) {
     alert ('это четное число')
